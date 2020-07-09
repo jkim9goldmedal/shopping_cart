bc82638195632dd0965f30a2d38b6d974e64b196
jkim9goldmedal <65585468+jkim9goldmedal@users.noreply.github.com>
2020-07-09 11:35:03 +0900

    Update show.py

--- show.py
+++ show.py
@@ -1,4 +1,20 @@
 def show():
     from execute import sql_execute
-    a = "select items.id,商品番号,商品名,商品詳細,販売単価,セール単価,均一単価,在庫数,セールフラグ,削除フラグ,"
+    c = "select 商品.id,商品番号,商品名,商品詳細,TRUNCATE(販売単価*(1+(税率/100)),0),販売単価,TRUNCATE(販売単価*(1-(割引率/100))*(1+(税率/100)),0),TRUNCATE(均一価格*(1+(税率/100)),0),均一単価,在庫数,セールフラグ,均一フラグ,削除フラグ\
+        from 商品 inner join 商品カテゴリー on 商品カテゴリーID = 書品カテゴリー.id inner join 税率 on 税率ID = 税率.id\
+        inner join 商品カテゴリー on 商品割引率ID = 商品割引率.id \
+        where 在庫数 > 0, 削除フラグ = 0\
+        group by 商品.id"
+    
+    a = "select id,カテゴリー from 商品カテゴリー where 削除フラグ = 0"
+    
+    rows = sql_execute(a)
+    
+    
+    print('2:商品情報検索・一覧')
+    print('カテゴリーID:カテゴリー名')
+    print(")
+    print('カテゴリーを表示します。')
+    for i in rows:
+          
     
