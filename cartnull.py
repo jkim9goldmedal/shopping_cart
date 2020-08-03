b33463ba26e8cd446a35f8f41d8a37bc52ee856e
jkim9goldmedal <65585468+jkim9goldmedal@users.noreply.github.com>
2020-08-03 22:47:09 +0900

    Delete cartnull.py

--- cartnull.py
+++
@@ -1,8 +0,0 @@
-def cartnull(x):
-    from execute import sql_execute
-    p = "select count(id) from カート where 顧客ID={} group by 顧客ID".format(x)
-    cartcount = sql_execute(p)
-
-    result = cartcount
-
-    return result
