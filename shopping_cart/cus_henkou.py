bcc697a8077aeb97f9cd20b731428351f1686224
jkim9goldmedal <65585468+jkim9goldmedal@users.noreply.github.com>
2020-08-14 09:08:41 +0900

    Update cus_henkou.py

--- shopping_cart/cus_henkou.py
+++ shopping_cart/cus_henkou.py
@@ -220,7 +220,7 @@ def henkou(x):
                 conn.commit()
 
                 update = "insert into 顧客(名前,カナ,郵便番号,住所,電話番号,配送料管理番号,メールアドレス,ログインID,ログインパスワード)\
-                     values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                     values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
                 cur.execute(update,[cus_list[1],cus_list[2],cus_list[3],cus_list[4],cus_list[5],cus_list[6],cus_list[7],cus_list[8],cus_list[9]])
                 conn.commit()
                 cur.close()
